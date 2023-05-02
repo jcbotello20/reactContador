ff727652b19dff2f5e62551156fa3bb57baa1c37
Camilo Botello <106172578+jcbotello20@users.noreply.github.com>
2023-05-01 19:20:44 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Counter from './App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Counter />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
